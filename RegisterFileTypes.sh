0573cda59d77914424138ae2cdebcc8f78f02bf6
X512 <danger_mail@list.ru>
2020-10-04 15:24:57 +0900

    RegisterFileTypes: add template for BlackBox Document

--- RegisterFileTypes.sh
+++ RegisterFileTypes.sh
@@ -1,3 +1,4 @@
 #!sh
 
 unzip -o Haiku/Rsrc/FileTypes.zip -d /boot/home/config/settings/mime_db/application
+cp Empty.odc "/boot/home/config/settings/Tracker/Tracker New Templates/BlackBox Document"
