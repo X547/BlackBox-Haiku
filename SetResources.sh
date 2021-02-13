20ce55302f7bd7eb8dfe56d8209f272415382cfc
X512 <danger_mail@list.ru>
2021-02-14 08:32:49 +0900

    Major update
    * Implement open, save and seve confirmation dialogs.
    * Fix cursors, add new cursors.
    * Change executable name from "Blackbox.so" to "Blackbox".
    * Fix or delete broken modules, now all modules are compiled fine.
    * Improve trap handling.
    * Fix nested HostSection's.
    * Misc changes.

--- SetResources.sh
+++ SetResources.sh
@@ -1,6 +1,5 @@
 #!sh
 
-chmod +x Blackbox.so
 rc Haiku/Rsrc/Blackbox.rdef -o Haiku/Rsrc/Blackbox.rsrc
-xres -o Blackbox.so Haiku/Rsrc/Blackbox.rsrc
-mimeset -f Blackbox.so
+xres -o Blackbox Haiku/Rsrc/Blackbox.rsrc
+mimeset -f Blackbox
