706f1ec36fe13654637161d818ed875ed60af9e8
X512 <danger_mail@list.ru>
2022-02-05 13:01:15 +0900

    Update

--- Launcher.sh
+++ Launcher.sh
@@ -1,3 +1,3 @@
 #!sh
 
-BB_WORK_DIR=`dirname "$0"` /boot/home/BlackBox-Haiku/Blackbox.so
+BB_WORK_DIR=`dirname "$0"` /boot/home/BlackBox-Haiku/Blackbox
--- SetResources.sh
+++ SetResources.sh
@@ -2,4 +2,4 @@
 
 rc Haiku/Rsrc/Blackbox.rdef -o Haiku/Rsrc/Blackbox.rsrc
 xres -o Blackbox Haiku/Rsrc/Blackbox.rsrc
-mimeset -f Blackbox
+mimeset -F Blackbox
